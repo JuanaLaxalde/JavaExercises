6c4262c2bcb4e8e1037d867997b5ca488f95db83
JuanaLaxalde <113561550+JuanaLaxalde@users.noreply.github.com>
2022-09-27 19:20:40 -0200

    Create Ex03.java

---
+++ Ex03.java
@@ -0,0 +1,17 @@
+// Java program to find the area of a triangle whose three sides are given
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TRIANGLE AREA");
+        System.out.println(areaOfTrian(10,12,16));
+    }
+}
+public static double areaOfTrian (double a, double b, double c){
+        ArrayList<Double> arr = new ArrayList<Double>();
+        arr.add(a);
+        arr.add(b);
+        arr.add(c);
+        arr.sort(Comparator.naturalOrder());
+        double result = (arr.get(0)*arr.get(1))*0.5;
+        return result;
+    }
