26fb02e2230f157b8c160ba5b5abc84ce8f411b2
JuanaLaxalde <113561550+JuanaLaxalde@users.noreply.github.com>
2022-09-27 19:24:43 -0200

    Create Ex05.java

---
+++ Ex05.java
@@ -0,0 +1,10 @@
+//5. Java program to find the product of a set of real numbers
+        
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(produceReals(5,9,85,123));
+    }
+}
+static int produceReals(int a, int b, int c, int d){
+        return a*b*c*d;
+    }
